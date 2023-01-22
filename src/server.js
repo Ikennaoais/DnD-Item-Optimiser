d9e9d84a316698618dc4eff2c8723b66a6bbad51
Ikenna <ikennaoais@gmail.com>
2023-01-22 18:28:02 +0000

    database setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,48 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes.js');
+const mongoose = require('mongoose');
+
+// Connect to the MongoDB database
+mongoose.connect("mongodb://localhost:27017/DarkAndDarker-Data", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+}).then(() =>
+    console.log("MongoDB Connected")
+);
+
+// Get the connection object
+const db = mongoose.connection;
+
+// Handle connection errors
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// Define the schema for the collection
+const equipmentSchema = new mongoose.Schema({
+    name: String,
+    class: [String],
+    quality: String,
+    ar_range: [Number],
+    agility: Number,
+    hs_reduction: Number,
+    mr: Number,
+    will: Number,
+    knowledge: Number,
+    strength: Number,
+    ms: Number,
+    projectile_reduction: Number,
+    slot: String
+});
+
+// Create a model for the collection
+const Equipment = mongoose.model('Equipment', equipmentSchema);
+
+// Retrieve all documents from the collection that have the class "Fighter"
+Equipment.find({ class: "Fighter" }, (err, equipment) => {
+    if (err) return console.error(err);
+    console.log(equipment);
+});
+
 //const middleware = require('./middleware');
 
 // Apply Middleware
